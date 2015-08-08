57746f06f74d06d3db1d08dd5be93c2d7995e425
Venkata Vitakula <prasad_vitakula@live.com>
2015-08-08 14:22:30 -0700

    Flatlanders

---
+++ Flatlanders/Scripts/app.js
@@ -0,0 +1,26 @@
+﻿
+
+(function () {
+
+    var app = angular.module("store", ['store-products']);
+
+    app.controller("StoreController", ['$http', function ($http) {
+        var store = this;
+
+        store.products = [];
+        $http.get("/store-products.json").success(function (data) {
+            store.products = data;
+        });
+    }]);
+
+    app.controller("ReviewController", function () {
+        this.review = {};
+        this.addReview = function (product) {
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
+    
+    
+
+})();
---
+++ Flatlanders/Scripts/products.js
@@ -0,0 +1,49 @@
+﻿(function () {
+    var productModule = angular.module("store-products", []);
+
+    productModule.directive("productTitle", function () {
+        return {
+            restrict: 'A',
+            templateUrl: 'product-title.html'
+        };
+    });
+
+    productModule.directive('productPanels', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-panels.html',
+            controller: function () {
+                this.selectedTab = 1; //Initialize the selectedTab.
+                this.selectTab = function (tabToSelect) {
+                    this.selectedTab = tabToSelect;
+                };
+                this.isSelected = function (tabToCheck) {
+                    return this.selectedTab === tabToCheck;
+                };
+            },
+            controllerAs: 'panel'
+        };
+    });
+
+    productModule.directive("productGallery", function () {
+        return {
+            restrict: "E",
+            templateUrl: "product-gallery.html",
+            controller: function () {
+                this.current = 0;
+                this.setCurrent = function (imageNumber) {
+                    this.current = imageNumber || 0;
+                };
+            },
+            controllerAs: "gallery"
+        };
+    });
+
+
+    productModule.directive("productSpecs", function () {
+        return {
+            restrict: "E",
+            templateUrl: "product-specs.html"
+        };
+    });
+})();
